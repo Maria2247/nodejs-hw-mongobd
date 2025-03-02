7fbde164356989821e44a9d2fd7c41725644d4fb
Mariia Savchenko <mashasavchenko@yahoo.com>
2025-03-02 16:09:02 +0200

    initial commit

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,43 @@ export const getContactById = async (contactId) => {
   const contact = await contactsCollection.findById(contactId);
   return contact;
 };
+
+export const createContact = async (payload) => {
+  payload = {
+    name: 'Jason Born',
+    phoneNumber: '+3800700777700',
+    email: 'jborn@thebest.com',
+    contactType: 'work',
+  };
+
+  const contact = await contactsCollection.create(payload);
+  return contact;
+};
+
+export const updateContact = async (contactId, payload, options = {}) => {
+  const rawResult = await contactsCollection.findByIdAndUpdate(
+    {
+      _id: contactId,
+    },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
+
+export const deleteContact = async (contactId) => {
+  const contact = await contactsCollection.findByIdAndDelete({
+    _id: contactId,
+  });
+  return contact;
+};
